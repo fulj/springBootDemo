7cdb24ee710e56ee0a95241ec3ee5d35051f4a17
liangjun.fu <fu.liangjun@trs.com.cn>
2019-07-08 13:33:08 +0800

    java reflect demo

---
+++ src/main/java/com/example/demo/ResolvableTypeTest/Parent.java
@@ -0,0 +1,20 @@
+package com.example.demo.ResolvableTypeTest;
+
+/**
+ * 测试继承获取泛型
+ *
+ * @author: wangji
+ * @date: 2018/06/25 20:34
+ */
+public class Parent<T> {
+}
+
+
+/**
+ * 测试泛型
+ *
+ * @author: wangji
+ * @date: 2018/06/25 20:42
+ */
+interface IParent<T> {
+}
---
+++ src/main/java/com/example/demo/ResolvableTypeTest/reflect/MainTest.java
@@ -0,0 +1,17 @@
+package com.example.demo.ResolvableTypeTest.reflect;
+
+public class MainTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		TestHelper.testConstructor();
+		TestHelper.testFiled();
+		TestHelper.testMethod();
+		TestHelper.testArrayClass();
+		TestHelper.getGenericType();
+
+	}
+
+}
